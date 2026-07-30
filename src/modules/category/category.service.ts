44bcf3953e5578dd5c1c320f26b68bfc94839711
Sarafat Hossen <sarafathossen01@gmail.com>
2026-07-30 11:18:01 +0600

    update categori service

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,6 +1,5 @@
 import { prisma } from "../../lib/prisma.js";
 
-// ১. নতুন ক্যাটাগরি তৈরি করা (শুধুমাত্র Admin এর জন্য)
 const createCategoryIntoDB = async (payload: { name: string }) => {
   const result = await prisma.category.create({
     data: payload,
@@ -8,12 +7,11 @@ const createCategoryIntoDB = async (payload: { name: string }) => {
   return result;
 };
 
-// ২. সব ক্যাটাগরি গেট করা (পাবলিক - সবাই দেখতে পারবে)
 const getAllCategoriesFromDB = async () => {
   const result = await prisma.category.findMany({
     include: {
       _count: {
-        select: { gearItems: true }, // কোন ক্যাটাগরিতে কয়টা প্রোডাক্ট আছে তাও দেখা যাবে
+        select: { gearItems: true }, 
       },
     },
   });
